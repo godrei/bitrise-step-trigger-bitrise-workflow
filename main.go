eaf59c964b1b88670b38412fb9deb939d7e0af06
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-04-05 20:36:35 +0200

    Update main.go

--- main.go
+++ main.go
@@ -129,7 +129,7 @@ func performRequest(request *http.Request) (ResponseModel, error) {
 		return responseModel, err
 	}
 
-	fmt.Println(contents)
+	fmt.Println(string(contents))
 	err = json.Unmarshal(contents, &responseModel)
 	return responseModel, err
 }
